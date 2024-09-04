1e5280296de850127b266985a1504d00d97703fc
Jhon Marreros Guzmán <j_marreros@hotmail.com>
2024-09-04 16:23:13 -0500

    Correccion consulta agrupada

--- includes/Database.php
+++ includes/Database.php
@@ -14,26 +14,24 @@ class Database {
 
 	public function get_courses_by_hint( string $courses_name_hint, string $start_date, string $end_date ): array {
 		$post_table     = $this->wpdb->posts;
-		$post_meta_table = $this->wpdb->postmeta;
 
 		if ( empty( $start_date )  || empty( $end_date ) ) {
 			return [];
 		}
 
-		$sql = "SELECT c.ID, c.post_title name FROM $post_table c
-				INNER JOIN $post_meta_table cm 
-				    ON c.ID = cm.post_id
+		$sql = "SELECT c.ID, c.post_title name, c.post_date create_date FROM $post_table c
 				WHERE 
 				c.post_type = 'stm-courses' AND 
-				cm.meta_key = '" . DCMS_COURSE_END_DATE . "' AND
-				DATE(cm.meta_value) BETWEEN '$start_date' AND '$end_date' AND
-				c.post_title LIKE '%$courses_name_hint%'";
+				DATE(c.post_date) BETWEEN '$start_date' AND '$end_date' AND
+				c.post_title LIKE '%$courses_name_hint%'
+				ORDER BY c.post_title, c.post_date";
 
 		return $this->wpdb->get_results( $sql, ARRAY_A );
 	}
 
 	public function get_students_by_courses($courses_ids): array {
 		$user_table     = $this->wpdb->users;
+		$post_table     = $this->wpdb->posts;
 		$user_meta_table = $this->wpdb->usermeta;
 		$user_courses_table = $this->wpdb->prefix . 'stm_lms_user_courses';
 
@@ -41,18 +39,23 @@ class Database {
 			return [];
 		}
 
-		$sql = "SELECT ID, 
+		$sql = "SELECT u.ID, 
 	                user_login, 
 	                display_name user_name, 
 	                user_email, 
-	                um.meta_value user_phone 
+	                um.meta_value user_phone,
+	                GROUP_CONCAT(c.post_title) courses
 				FROM $user_table u
 				INNER JOIN (
-						SELECT DISTINCT user_id FROM $user_courses_table 
+						SELECT DISTINCT user_id, course_id FROM $user_courses_table 
                         WHERE course_id IN (" . implode(',', $courses_ids) . ")
 				) uc ON u.ID = uc.user_id
+				INNER JOIN $post_table c ON c.ID = uc.course_id
 				LEFT JOIN $user_meta_table um ON u.ID = um.user_id AND meta_key = 'billing_phone'
+				GROUP BY u.ID, user_login, user_name, user_email, user_phone
 				ORDER BY user_name";
+
+		error_log(print_r($sql,true));
 
 		return $this->wpdb->get_results( $sql, ARRAY_A );
 	}
